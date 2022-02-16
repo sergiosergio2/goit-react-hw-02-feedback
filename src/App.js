182cbc71518e2bd286310292055f8505c66320e0
sergiosergio2 <vzdor5555@gmail.com>
2022-02-16 15:33:52 +0200

    added statistics, without styles

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import FeedbackOptions from './components/feedbackOption';
+import Statistics from 'components/statistic';
 
 
 
@@ -9,15 +10,34 @@ class App extends Component {
         neutral: 0,
         bad: 0,
 
+    };
+    onLeaveFeedback = evt => {
+        const click = evt.currentTarget.click;
+        this.setState(prevState => ({[click]: prevState[click] + 1,}))
+
+    }
+
+    countTotalFeedback() {
+        return this.state.good + this.state.neutral + this.state.bad;
     }
 
     render() {
         return (
-        <>
+       <>
         <FeedbackOptions
-        options={['good', 'neutral', 'bad']} onLeaveFeedback={console.log}>
+        options={['good', 'neutral', 'bad']} onLeaveFeedback={this.onLeaveFeedback}>
         </FeedbackOptions>
+   
+    
+          <Statistics
+          good={this.state.good}
+          neutral={this.state.neutral}
+          bad={this.state.bad}
+          total={this.countTotalFeedback()}
+          
+        ></Statistics>
         </>
+    
         );
     }
 }
---
+++ src/components/statistic/Statistics.js
@@ -0,0 +1,30 @@
+import PropTypes from 'prop-types';
+import s from './Statistics.module.css';
+
+const Statistics = ({
+  good,
+  neutral,
+  bad,
+  total,
+  
+}) => {
+  return (
+<div>
+  <p className={s.text}>Good:{good}</p>
+  <p className={s.text}>Neutral:{neutral}</p>
+  <p className={s.text}>Bad:{bad}</p>
+  <p className={s.text}>Total:{total}</p>
+  </div>
+  )
+}
+
+
+
+Statistics.propTypes = {
+  good:PropTypes.number.isRequired,
+  neutral:PropTypes.number.isRequired,
+  bad:PropTypes.number.isRequired,
+  total:PropTypes.number.isRequired,
+  
+};
+export default Statistics;
